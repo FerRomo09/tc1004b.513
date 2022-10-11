606b6d1b718026a544508c801ca4fa2541f4083a
FerRomo09 <fernandaeliromo@hotmail.com>
2022-10-11 13:27:38 +0000

    Practica 2

---
+++ signal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+int contador;
+
+void holaMundo(int signalNumber){
+    if (signalNumber = 10){
+        printf("Es la senal 10\n");
+        contador == 2;
+    } else {
+        printf("Es otra senal\n");
+    }
+    
+}  /* Solo afecta en el proceso */
+
+void nomeMatas(int sig){
+    printf("Es la senal 10\n");
+}
+
+int main(){
+    signal(12, holaMundo);
+    signal(2, nomeMatas);
+    signal(10, nomeMatas);
+    contador = 1;
+    while(contador == 1){
+        printf("Estoy trabajando\n");
+        sleep(1);
+    }
+    printf("Nunca llega\n"); /*Proceso infinito while*/
+    return 0;
+}
